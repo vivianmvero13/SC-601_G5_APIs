4089b807c6693b8cb8356b28c117e5a4c779f613
Vivian Velazquez <vivianmvero@gmail.com>
2025-07-16 16:03:21 -0600

    Model

---
+++ API/CafeteriaEspresso/Models/DistritoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CafeteriaEspresso.Models
+{
+    public class DistritoModel
+    {
+        public int id { get; set; } // [pk, increment]
+        public string nombre { get; set; }
+        public int id_canton { get; set; } // [fk, not null]
+    }
+}
