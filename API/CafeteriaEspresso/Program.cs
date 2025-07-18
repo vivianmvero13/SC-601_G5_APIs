ab8c151f672f23201ffa1d95520c79fc562712bb
Fabian-AM <fabian.alonso238@gmail.com>
2025-07-17 22:49:25 -0600

    Rol-Usuarios-Clientes-Colaboradores
    Rol-Usuarios-Clientes-Colaboradores

--- API/CafeteriaEspresso/Program.cs
+++ API/CafeteriaEspresso/Program.cs
@@ -32,8 +32,13 @@ builder.Services.AddScoped<CategoriaProductosService>();
 builder.Services.AddScoped<InventarioService>();
 builder.Services.AddScoped<ProveedoresService>();
 builder.Services.AddScoped<DetalleFacturaService>();
-builder.Services.AddScoped<EstadosService>();
 builder.Services.AddScoped<UsuarioService>();
+builder.Services.AddScoped<ClienteService>();
+builder.Services.AddScoped<ColaboradoresService>();
+builder.Services.AddScoped<RolesService>();
+builder.Services.AddScoped<EstadosService>();
+
+
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
