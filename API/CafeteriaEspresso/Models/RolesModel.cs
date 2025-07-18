1ae585d65cd5d48a8950ebe2231c22955ee18ba1
Fabian-AM <fabian.alonso238@gmail.com>
2025-07-17 22:46:18 -0600

    Rol-Usuarios-Clientes-Colaboradores Completos
    Rol-Usuarios-Clientes-Colaboradores Completos

---
+++ API/CafeteriaEspresso/Models/RolesModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CafeteriaEspresso.Models
+{
+    public class RolesModel
+    {
+        [Key]
+        public int id { get; set; }
+        public string nombre { get; set; }
+       
+    }
+}
